1da568706109419497dc21ecb05284c55667eebb
zhangzhibang0309 <295854713@qq.com>
2021-08-21 19:06:55 +0800

    更新

---
+++ RegistrationFE/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img class="logo" alt="Orange logo" src="https://catalinazzz.oss-cn-beijing.aliyuncs.com/image/DZJFMt.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scroped>
+  .logo {
+    width: 30%;
+  }
+</style>
