c5cb1141eebac7b058b8157a4c072950075d4873
zhangzhibang <zhangzhibang@exiao.tech>
2022-08-16 01:27:38 +0800

    feat: 清除bar的vuetify依赖

--- RegistrationFE/src/components/app/NavigationBar.vue
+++ RegistrationFE/src/components/app/NavigationBar.vue
@@ -1,57 +1,6 @@
 <template>
   <div>
-    <!-- 导航栏 -->
-    <v-toolbar
-      dark
-      prominent
-      class="back"
-      src="https://catalinazzz.oss-cn-beijing.aliyuncs.com/image/20210822202535.png?x-oss-process=image/format,jpg/interlace,1"
-      style="background-position: center 0%"
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Orange Studio </v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
-
-    <!-- 侧边抽屉 -->
-    <v-navigation-drawer
-      app
-      bottom
-      v-model="drawer"
-      temporary
-      src="https://catalinazzz.oss-cn-beijing.aliyuncs.com/image/ING_1901.png?x-oss-process=image/format,jpg/interlace,1"
-    >
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img
-            src="https://catalinazzz.oss-cn-beijing.aliyuncs.com/image/6.6.png?x-oss-process=image/format,jpg/interlace,1"
-          ></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title style="color: white">Orange Studio</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item
-          @click="drawer = false"
-          v-for="item in items"
-          :key="item.title"
-          :to="item.link"
-        >
-          <v-list-item-icon>
-            <v-icon style="color: white">{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title style="color: white">{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <router-link v-for="item in items" :to="item.link">{{ item.title }}--</router-link>
   </div>
 </template>
 
@@ -59,7 +8,6 @@
 export default {
   name: "NavigationBarHead",
   data: () => ({
-    drawer: false,
     items: [
       { title: "主页", icon: "mdi-view-dashboard", link: "/" },
       { title: "关于橙果", icon: "mdi-forum", link: "/about" },
@@ -67,9 +15,12 @@ export default {
       { title: "查看录取进度", icon: "mdi-loading", link: "/status" },
     ],
   }),
-  methods: {},
-  created() {},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+a {
+  text-decoration: none;
+  color: #333;
+}
+</style>
