8020f67a25566c917916caf65e6a04b33df1d94b
zhangzhibang <295854713c@gmail.com>
2022-08-19 02:56:39 +0800

    feat: 设置icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,23 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 export default defineNuxtConfig({
+  meta: {
+    title: "橙果工作室",
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
+    ],
+  },
   nitro: {
     storage: {
-      'redis': {
-        driver: 'redis',
+      redis: {
+        driver: "redis",
         /* redis connector options */
         port: 6379, // Redis port
         host: "127.0.0.1", // Redis host
         username: "", // needs Redis >= 6
         password: "",
-        db: 0 // Defaults to 0
-      }
-    }
-  }
-})
+        db: 0, // Defaults to 0
+      },
+    },
+  },
+});
