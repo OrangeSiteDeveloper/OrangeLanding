0d0ac668904f9bc1d6a31ac70aba29809839b39d
zhangzhibang <295854713c@gmail.com>
2022-08-19 03:55:28 +0800

    feat: 安装naive

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,56 @@
 <template>
   <div>
-    {{ a }}
+    <n-space>
+      <n-button strong secondary> Default </n-button>
+      <n-button strong secondary type="tertiary"> Tertiary </n-button>
+      <n-button strong secondary type="primary"> Primary </n-button>
+      <n-button strong secondary type="info"> Info </n-button>
+      <n-button strong secondary type="success"> Success </n-button>
+      <n-button strong secondary type="warning"> Warning </n-button>
+      <n-button strong secondary type="error"> Error </n-button>
+      <n-button strong secondary round> Default </n-button>
+      <n-button strong secondary round type="primary"> Primary </n-button>
+      <n-button strong secondary round type="info"> Info </n-button>
+      <n-button strong secondary round type="success"> Success </n-button>
+      <n-button strong secondary round type="warning"> Warning </n-button>
+      <n-button strong secondary round type="error"> Error </n-button>
+      <n-button strong secondary circle>
+        <template #icon>
+          <n-icon><cash /></n-icon>
+        </template>
+      </n-button>
+      <n-button strong secondary circle type="primary">
+        <template #icon>
+          <n-icon><cash /></n-icon>
+        </template>
+      </n-button>
+      <n-button strong secondary circle type="info">
+        <template #icon>
+          <n-icon><cash /></n-icon>
+        </template>
+      </n-button>
+      <n-button strong secondary circle type="success">
+        <template #icon>
+          <n-icon><cash /></n-icon>
+        </template>
+      </n-button>
+      <n-button strong secondary circle type="warning">
+        <template #icon>
+          <n-icon><cash /></n-icon>
+        </template>
+      </n-button>
+      <n-button strong secondary circle type="error">
+        <template #icon>
+          <n-icon><cash /></n-icon>
+        </template>
+      </n-button>
+    </n-space>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-
-let a = ref(1);
+import { NButton, NSpace, NIcon } from "naive-ui";
+import { Cash } from "@vicons/ionicons5";
 </script>
 
 <style scoped>
