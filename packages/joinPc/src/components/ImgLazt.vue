bf637d7edea4147b0adecfada5aa7b42d6e9dd14
zhangzhibang <295854713c@gmail.com>
2022-09-01 20:36:01 +0800

    feat: 编写图片懒加载组件

---
+++ packages/joinPc/src/components/ImgLazt.vue
@@ -0,0 +1,40 @@
+<template>
+  <img ref="image" :src="tmp.src" :date-src="tmp.data_src">
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const props = defineProps(["value"]);
+let tmp = ref(props.value);
+
+
+
+const image = ref()
+function throttle(fn: { (): void; (): void; }, delay: number | undefined) {
+  let flag = true;
+  return () => {
+    if (!flag) return;
+    flag = false;
+    const timer = setTimeout(() => {
+      fn();
+      flag = true;
+    }, delay);
+  };
+}
+
+
+onMounted(() => {
+  let img = image.value
+
+  window.addEventListener(
+    "scroll",
+    throttle(() => {
+      const imageTop = img.getBoundingClientRect().top;
+      if (imageTop < window.innerHeight) {
+        tmp.value.src = tmp.value.data_src;
+      }
+    }, 1000)
+  );
+})
+</script>
